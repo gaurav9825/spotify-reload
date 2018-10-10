a47ebc381f57940c1a33ba6cd42cd06e0e92ac03
Gaurav Kumar <gaurav.kumar9825@gmail.com>
2018-10-10 17:08:58 +0530

    Updates

---
+++ precache-manifest.ce0b0758bbde2c3fd243625c8e2e1761.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35442ea7a5bb3f2cb931",
+    "url": "/spotify-reload/static/js/runtime~main.35442ea7.js"
+  },
+  {
+    "revision": "9c7a16e2fa468557472a",
+    "url": "/spotify-reload/static/js/main.9c7a16e2.chunk.js"
+  },
+  {
+    "revision": "0077b1aede5f396fea68",
+    "url": "/spotify-reload/static/js/1.0077b1ae.chunk.js"
+  },
+  {
+    "revision": "9c7a16e2fa468557472a",
+    "url": "/spotify-reload/static/css/main.5bd19495.chunk.css"
+  },
+  {
+    "revision": "c6ca2b1e6fa7e00439da4b355d43e307",
+    "url": "/spotify-reload/index.html"
+  }
+];
